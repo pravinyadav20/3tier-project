d254fdddb6bcd254381f8ce9a164300091f85589
pravinyadav20 <pravinbaijnathyadav@gmail.com>
2025-07-05 15:28:48 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-2.c1es6aago69m.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Pravin1234',
+    DB_DATABASE : 'webappdb'
 });
