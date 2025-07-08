3422331fc047d6964fe5e96587a1158155076d2b
pravinyadav20 <pravinbaijnathyadav@gmail.com>
2025-07-08 12:17:28 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -4,7 +4,12 @@ echo "Linking custom nginx config..."
 sudo ln -sf /etc/nginx/sites-available/react-backend /etc/nginx/sites-enabled/react-backend
 
 # Optional: Remove default config if needed
-sudo rm -f /etc/nginx/sites-enabled/default
+# sudo rm -f /etc/nginx/sites-enabled/default
+      - echo "Running Docker containers..."
+      - docker run -d --name frontend-container -p 81:80 frontend-image
+      - docker run -d --name backend-container -p 4000:4000 backend-image
+      - echo "Docker containers started successfully."
+
 
 echo "Testing nginx config..."
 sudo nginx -t
